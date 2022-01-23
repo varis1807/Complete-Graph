c2ea92e566225a980fb54773a2f38ca1d51d0b96
Varis Rana <vrvarisrana123@gmail.com>
2022-01-23 09:16:18 +0530

    commit 23

--- Breadth First Search/graph.java
+++ Breadth First Search/graph.java
@@ -6,8 +6,12 @@ class Edge {
       int wt;
 
       Edge(int src, int nbr, int wt) {
-           this.src=src;
-           this.nbr=nbr;
-           this.wt=wt;
+            this.src = src;
+            this.nbr = nbr;
+            this.wt = wt;
       }
+}
+
+public class graph {
+
 }
