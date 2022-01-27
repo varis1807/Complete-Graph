2094047fc139948ed4674a731e7da27ea3eb67e1
Varis Rana <vrvarisrana123@gmail.com>
2022-01-27 10:46:48 +0530

    commit 27

--- Deapth First Search/directedGraph.java
+++ Deapth First Search/directedGraph.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class directedGraph {
-      public static void addedge(ArrayList<Edge>[] graph, int u, int w) {
-
+      public static void addedge(ArrayList<Edge>[] graph, int u, int v, int w) {
+            graph[u].add(new Edge(u, v, w));
       }
 }
