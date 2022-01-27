94832e92d830f23799a85e9758104e1e4abeba55
Varis Rana <vrvarisrana123@gmail.com>
2022-01-27 18:05:53 +0530

    commit 27

--- Deapth First Search/topologicalSort.java
+++ Deapth First Search/topologicalSort.java
@@ -1,4 +1,31 @@
 import java.util.*;
 public class topologicalSort {
-      
+      public void dfs_topo(int src,ArrayList<Edge>[] graph,boolean[] vis,ArrayList<Integer> topo){
+            vis[src]=true;
+    
+            for(Edge e:graph[src]){
+                if(!vis[e.nbr]){
+                    dfs_topo(e.nbr,graph,vis,topo);
+                }
+            }
+    
+            topo.add(src);
+        }
+    
+        public void topological_order(ArrayList<Edge>[] graph){
+            int V=graph.length;
+    
+            boolean[] vis=new boolean[V];
+            ArrayList<Integer> topo=new ArrayList<>();
+            for(int i=0; i<V; i++){
+                if(!vis[i]){
+                    dfs_topo(i,graph,vis,topo);
+                }
+            }
+    
+            for(int i=topo.size()-1; i>=0; i--){
+                System.out.println(topo.get(i));
+            }
+        }
+     
 }
