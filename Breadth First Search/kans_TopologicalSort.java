9126722742f94f217267aa42868e6f07767c5a67
Varis Rana <vrvarisrana123@gmail.com>
2022-01-28 19:06:01 +0530

    commit 28

--- Breadth First Search/kans_TopologicalSort.java
+++ Breadth First Search/kans_TopologicalSort.java
@@ -1,4 +1,40 @@
 import java.util.*;
 public class kans_TopologicalSort {
-      
+      public static 
+
+      public static void addedge(ArrayList<Edge>[] graph, int u, int v, int w) {
+            graph[u].add(new Edge(u, v, w));
+      }
+
+      public static void display(ArrayList<Edge>[] graph) {
+            int N = graph.length;
+            for (int i = 0; i < N; i++) {
+                  System.out.print(i + "-> ");
+                  for (Edge e : graph[i]) {
+                        System.out.print("( " + e.nbr + " @ " + e.wt + " )");
+                  }
+                  System.out.println();
+            }
+      }
+
+      public static void construction() {
+            int N = 7;
+            ArrayList<Edge>[] graph = new ArrayList[7];
+            for (int i = 0; i < N; i++) {
+                  graph[i] = new ArrayList<>();
+            }
+            addedge(graph, 0, 1, 10);
+            addedge(graph, 0, 3, 20);
+            addedge(graph, 1, 2, 30);
+            addedge(graph, 2, 3, 40);
+            addedge(graph, 4, 3, 50);
+            addedge(graph, 4, 5, 60);
+            addedge(graph, 4, 6, 70);
+            // display(graph);
+           // topological_order(graph);
+      }
+
+      public static void main(String[] args) {
+            construction();
+      }
 }
