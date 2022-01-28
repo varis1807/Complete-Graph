0106a700495ac70c24b293a58268ad454df582b5
Varis Rana <vrvarisrana123@gmail.com>
2022-01-28 09:37:56 +0530

    commit 28

--- Deapth First Search/journeyToMoon.java
+++ Deapth First Search/journeyToMoon.java
@@ -1,41 +1,42 @@
 import java.util.*;
+
 public class journeyToMoon {
-      public static int dfsJourney(int src, List<List<Integer>> graph, boolean[] vis){
-      vis[src] = true;
-      int size = 0;
-      for(int e:graph.get(src)){
-              if(!vis[e]){
-                  size += dfsJourney(e, graph, vis);
-              }
+      public static int dfsJourney(int src, List<List<Integer>> graph, boolean[] vis) {
+            vis[src] = true;
+            int size = 0;
+            for (int e : graph.get(src)) {
+                  if (!vis[e]) {
+                        size += dfsJourney(e, graph, vis);
+                  }
+            }
+            return size + 1;
       }
-      return size+1;
-  }
-  
-  public static long journeyToMoon(int n, List<List<Integer>> astronaut) {
-  // Write your code here
-      List<List<Integer>> graph = new ArrayList<>();
-      
-      for(int i=0;i<n;i++){
-          graph.add(new ArrayList<Integer>());
+
+      public static long journeyToMoon(int n, List<List<Integer>> astronaut) {
+            // Write your code here
+            List<List<Integer>> graph = new ArrayList<>();
+
+            for (int i = 0; i < n; i++) {
+                  graph.add(new ArrayList<Integer>());
+            }
+            // making the graph according to the list given
+            for (int i = 0; i < astronaut.size(); i++) {
+                  List<Integer> ast = astronaut.get(i);
+                  graph.get(ast.get(0)).add(ast.get(1));
+                  graph.get(ast.get(1)).add(ast.get(0));
+            }
+
+            boolean[] vis = new boolean[n];
+            long ans = 0, totalSize = 0, size = 0;
+
+            for (int i = 0; i < n; i++) {
+                  if (!vis[i]) {
+                        size = dfsJourney(i, graph, vis);
+                        ans = ans + totalSize * size;
+                        totalSize += size;
+                  }
+            }
+            return ans;
       }
-      // making the graph according to the list given
-      for(int i=0;i<astronaut.size();i++){
-          List<Integer> ast = astronaut.get(i);
-          graph.get(ast.get(0)).add(ast.get(1));
-          graph.get(ast.get(1)).add(ast.get(0));
-      }
-      
-      boolean[] vis = new boolean[n];
-      long ans = 0, totalSize = 0, size=0;
-      
-      for(int i=0;i<n;i++){
-          if(!vis[i]){
-              size = dfsJourney(i,graph,vis);
-              ans = ans + totalSize * size;
-              totalSize += size;
-          }
-      }
-      return ans;
-  }
 
 }
