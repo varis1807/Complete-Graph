ffd3fd406c8455ad9402d5b5c75a6b4501738e1e
Varis Rana <vrvarisrana123@gmail.com>
2022-01-28 19:23:06 +0530

    commit 28

--- Breadth First Search/kans_TopologicalSort.java
+++ Breadth First Search/kans_TopologicalSort.java
@@ -1,13 +1,30 @@
 import java.util.*;
 
 public class kans_TopologicalSort {
-      public static void topoSort(ArrayList<Edge>[] graph) {
+      public static ArrayList<Integer> topoSort(ArrayList<Edge>[] graph) {
             int vrtx = graph.length;
             int[] inDgree = new int[vrtx];
             for (int i = 0; i < vrtx; i++) {
                   for (Edge e : graph[i])
                         inDgree[e.nbr]++;
             }
+            LinkedList<Integer> que = new LinkedList<>();
+            ArrayList<Integer> topo_order = new ArrayList<>();
+            for (int i = 0; i < vrtx; i++) {
+                  if (inDgree[i] == 0)
+                        que.addLast(i);
+            }
+
+            while (que.size() > 0) {
+                  int rvtx = que.removeFirst();
+                  topo_order.add(rvtx);
+                  for (Edge e : graph[rvtx]) {
+                        inDgree[e.nbr]--;
+                        if (inDgree[e.nbr] == 0)
+                              que.addLast(e.nbr);
+                  }
+            }
+            return topo_order;
       }
 
       public static void addedge(ArrayList<Edge>[] graph, int u, int v, int w) {
@@ -40,6 +57,7 @@ public class kans_TopologicalSort {
             addedge(graph, 4, 6, 70);
             // display(graph);
             // topological_order(graph);
+            System.out.println(topoSort(graph));
       }
 
       public static void main(String[] args) {
