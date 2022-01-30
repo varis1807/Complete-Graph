db7f72832b4aae3d02f3c9d571bc7c22b2d0b40a
Varis Rana <vrvarisrana123@gmail.com>
2022-01-30 08:13:41 +0530

    commit 30

--- UnionFind DisjointSet/smallestEquivalentStr.java
+++ UnionFind DisjointSet/smallestEquivalentStr.java
@@ -1,40 +1,43 @@
 import java.util.*;
+
 public class smallestEquivalentStr {
       static int[] par;
       static int[] size;
-      public static int findPar(int u){
-            if(par[u]==u)
-            return u;
-            return par[u]=
+
+      public static int findPar(int u) {
+            if (par[u] == u)
+                  return u;
+            return par[u] = findPar(par[u]);
       }
+
       public static String smallestEquivalentString(string s1, string s2, string baseStr) {
-            par=new int[26];
-        
-            for(int i=0; i<26; i++){
-                par[i]=i;
-            }        
-        
-            for(int i=0; i<s1.size(); i++){
-                int u=s1[i]-'a';
-                int v=s2[i]-'a';
-        
-                int p1=findPar(u);
-                int p2=findPar(v);
-        
-                if(p1!=p2){
-                    par[p2]=min(p1,p2);
-                    par[p1]=min(p1,p2);
-                }
+            par = new int[26];
+
+            for (int i = 0; i < 26; i++) {
+                  par[i] = i;
             }
-        
-            string ans="";
-            for(int i=0; i<baseStr.size(); i++){
-                int u=baseStr[i]-'a';
-        
-                int p=findPar(u);
-                ans=ans+(char)('a'+p);
+
+            for (int i = 0; i < s1.size(); i++) {
+                  int u = s1[i] - 'a';
+                  int v = s2[i] - 'a';
+
+                  int p1 = findPar(u);
+                  int p2 = findPar(v);
+
+                  if (p1 != p2) {
+                        par[p2] = min(p1, p2);
+                        par[p1] = min(p1, p2);
+                  }
             }
-        
+
+            string ans = "";
+            for (int i = 0; i < baseStr.size(); i++) {
+                  int u = baseStr[i] - 'a';
+
+                  int p = findPar(u);
+                  ans = ans + (char) ('a' + p);
+            }
+
             return ans;
-        }
+      }
 }
