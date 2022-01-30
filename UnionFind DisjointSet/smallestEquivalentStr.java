523bdb2a51980cf0c98118ebed87eecf1ce33b4a
Varis Rana <vrvarisrana123@gmail.com>
2022-01-30 08:10:04 +0530

    commit 30

--- UnionFind DisjointSet/smallestEquivalentStr.java
+++ UnionFind DisjointSet/smallestEquivalentStr.java
@@ -1,4 +1,33 @@
 import java.util.*;
 public class smallestEquivalentStr {
-      
+      string smallestEquivalentString(string s1, string s2, string baseStr) {
+            par.resize(26);
+        
+            for(int i=0; i<26; i++){
+                par[i]=i;
+            }        
+        
+            for(int i=0; i<s1.size(); i++){
+                int u=s1[i]-'a';
+                int v=s2[i]-'a';
+        
+                int p1=findPar(u);
+                int p2=findPar(v);
+        
+                if(p1!=p2){
+                    par[p2]=min(p1,p2);
+                    par[p1]=min(p1,p2);
+                }
+            }
+        
+            string ans="";
+            for(int i=0; i<baseStr.size(); i++){
+                int u=baseStr[i]-'a';
+        
+                int p=findPar(u);
+                ans=ans+(char)('a'+p);
+            }
+        
+            return ans;
+        }
 }
