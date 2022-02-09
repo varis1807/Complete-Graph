d42c8f838077b341b0add8c2c9418909bf93fa02
Varis Rana <vrvarisrana123@gmail.com>
2022-02-09 09:57:22 +0530

    commit 09

--- UnionFind DisjointSet/Spanning Tree/noOfIsland2.java
+++ UnionFind DisjointSet/Spanning Tree/noOfIsland2.java
@@ -4,41 +4,40 @@ public class noOfIsland2 {
       
 // num of islands 2======================================
 
-vector<int> par;
-int findPar(int u){
+int[] par;
+public int findPar(int u){
     if(par[u]==u) return u;
 
     return par[u]=findPar(par[u]);
 }
 
-vector<int> numIslands2(int n, int m, vector<vector<int>>& positions) {
-    vector<vector<int>> grid(n,vector<int>(m,0));
+public List<Integer> numIslands2(int n, int m, int[][] positions) {
+    int[][] grid=new int[n][m];
 
-    par.resize(n*m);
+    par=new int[n*m];
     for(int i=0; i<n*m; i++){
         par[i]=i;
     }
 
+    int[][] dirs={{0,1},{1,0},{0,-1},{-1,0}};
+
+    List<Integer> ans=new ArrayList<>();
     int count=0;
-    vector<int> ans;
+    for(int[] pos:positions){
+        int x=pos[0];
+        int y=pos[1];
 
-    vector<vector<int>> dirs={{0,1},{1,0},{0,-1},{-1,0}};
-
-    for(int i=0; i<positions.size(); i++){
-        int px=positions[i][0];
-        int py=positions[i][1];
-
-        if(grid[px][py]==1){
-            ans.push_back(count);
+        if(grid[x][y]==1){
+            ans.add(count);
             continue;
         }
 
-        grid[px][py]=1;
+        grid[x][y]=1;
         count++;
-        int p1=findPar(px*m+py);
-        for(auto &dir:dirs){
-            int i=px+dir[0];
-            int j=py+dir[1];
+        int p1=findPar(x*m+y);
+        for(int[] dir:dirs){
+            int i=x+dir[0];
+            int j=y+dir[1];
 
             if(i>=0 && j>=0 && i<n && j<m && grid[i][j]==1){
                 int p2=findPar(i*m+j);
@@ -49,54 +48,9 @@ vector<int> numIslands2(int n, int m, vector<vector<int>>& positions) {
                 }
             }
         }
-        ans.push_back(count);
+        ans.add(count);
     }
 
     return ans;
-}
-
-// num of islands 2 ===========================================================
-vector<int> par;
-int findPar(int u){
-    return par[u]==u ? u : par[u]=findPar(par[u]);
-}
-
-vector<int> numIslands2(int n, int m, vector<vector<int>>& positions) {
-    par.resize(n*m,-1); // -1 -> water
-
-    int count=0;
-    vector<int> ans;
-
-    vector<vector<int>> dirs={{0,1},{1,0},{0,-1},{-1,0}};
-
-    for(vector<int>& pos:positions){
-        int px=pos[0];
-        int py=pos[1];
-
-        if(par[px*m+py]!=-1){
-            ans.push_back(count);
-            continue;
-        }
-
-        par[px*m+py]=px*m+py;
-        count++;
-        int p1=findPar(px*m+py);
-
-        for(auto &dir:dirs){
-            int i=px+dir[0];
-            int j=py+dir[1];
-
-            if(i>=0 && j>=0 && i<n && j<m && par[i*m+j]!=-1){
-                int p2=findPar(i*m+j);
-
-                if(p1!=p2){
-                    par[p2]=p1;
-                    count--;
-                }
-            }
-        }
-        ans.push_back(count);
-    }
-    return ans;    
 }
 }
